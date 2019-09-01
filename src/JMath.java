5e511aab31c9f8bd43c8da6c84c6b58ac70157d9
Jmannn <manjo773@student.otago.ac.nz>
2019-09-01 21:44:26 +0100

    added some array manipulation and worked on RNN

---
+++ src/JMath.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+/* Class for performing functions normally done by NumPy. */
+public class JMath {
+    JMath(){
+
+    };
+
+    /* Used to transpose a java array */
+    public static double[][] transpose(double[][] array){
+        double[][] arrayT = new double[array[0].length][array.length];
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[0].length; j++){
+                arrayT[j][i] = array[i][j];
+            }
+        }
+        return arrayT;
+    }
+    /* Initializes a 2d array of doubles with random values */
+    public static double[][] randomSample2D(int x, int y){
+        Random random = new Random();
+        double[][] newArray = new double[x][y];
+        for (int i = 0; i < x; i++){
+            for (int j = 0; j < y; y++){
+                newArray[i][j] = random.nextGaussian();
+            }
+        }
+        return newArray;
+    }
+    /* Concatenates two multi dimensional arrays */
+    public static double[][] vStack(double[][] arr1, double[][] arr2){
+        double[][] vStacked = new double[arr1.length+arr2.length][arr1[0].length];
+        for(int i = 0 ; i < arr1.length; i++){
+            vStacked[i] = arr1[i];
+        }
+        for(int i = arr1.length; i<arr1.length+arr2.length; i++){
+            vStacked[i] = arr2[i-arr1.length];
+        }
+        return vStacked;
+    }
+
+}
--- src/RNN.java
+++ src/RNN.java
@@ -1,7 +1,58 @@
 public class RNN {
 
+    // timesteps is like number of pixels in a picture,
+
+    /* Class members */
+
+    /* Initial input */
+    double[] x;
+    /* Expected output, shifted by a timestep */
+    double[] y;
+    /* Input size */
+    int input;
+    /* Output Size */
+    int output;
+    /* Weight matrix for interpreting results from the hidden weight matrix */
+    double[][] w;
+    /*Matrix used in RMSprop to decay the learning rate */
+    double[][] g;
+    /* Number of recurrences i.e number of inputs , number of chars*/
+    int recurrences;
+    /* Learing rate */
+    double learningRate;
+    /* Array for storing inputs over recurrences */
+    double[][] ia;
+    /* For storing cell states */
+    double[][] ca;
+    /* For storing outputs */
+    double[][] oa;
+    /* For storing hidden states */
+    double[][] ha;
+    /* Forget gate */
+    double[][] af;
+    /* Input gate */
+    double[][] ai;
+    /* Cell state*/
+    double[][] ac;
+    /* Output gate */
+    double[][] ao;
+    /* Expected output array 2d */
+    double[][] expectedOutput;
     RNN(int input, int output, int recurrences, double[][] expectedOutput ,double learningRate){
         //.T means to transpose
+        this.x = new double[input];
+        this.input = input;
+        this.y = new double[output];
+        this.output = output;
+        this.w = JMath.randomSample2D(output, output);
+        this.g = new double[output][output];
+        this.recurrences = recurrences;
+        this.learningRate = learningRate;
+
+        //self.expected_output = np.vstack((np.zeros(expected_output.shape[0]), expected_output.T))
+        // expected_output.shape[0] = expectedOutput.length
+        //vstack just stacks em together by the row or fist []
 
     };
+
 }
